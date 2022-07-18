0f29453da04684a0caeee7b4083f6fdb135ad1fb
jonahBerlyne <jonshaber@gmail.com>
2022-07-18 13:50:23 -0400

    added express

--- index.js
+++ index.js
@@ -1,40 +1,68 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
 
-const server = http.createServer((req, res) => {
- res.setHeader('Content-Type', 'text/html');
+const app = express();
 
- let path = './public/';
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
 
- switch (req.url) {
-  case '/':
-   path += 'index.html';
-   res.statusCode = 200;
-   break;
-  case '/about':
-   path += 'about.html';
-   res.statusCode = 200;
-   break;
-  case '/contact-me':
-   path += 'contact-me.html';
-   res.statusCode = 200;
-   break;
-  default:
-   path += '404.html';
-   res.statusCode = 404;
-   break;
- }
-
- fs.readFile(path, (err, data) => {
-  if (err) {
-   if (err.code !== 'ENOENT') {
-    res.writeHead(500);
-    res.end(`Server Error: ${err.code}`);
-   }
-  } else {
-   res.end(data);
-  }
- });
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-server.listen(8080, () => console.log("Server running on port 8080..."));
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/about.html'));
+});
+
+app.get('/contact-me', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/contact-me.html'));
+});
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, './public/404.html'));
+});
+
+app.listen(8080, () => console.log("Server running on port 8080..."));
+
+// const http = require('http');
+// const fs = require('fs');
+
+// const server = http.createServer((req, res) => {
+//  res.setHeader('Content-Type', 'text/html');
+
+//  let path = './public/';
+
+//  switch (req.url) {
+//   case '/':
+//    path += 'index.html';
+//    res.statusCode = 200;
+//    break;
+//   case '/about':
+//    path += 'about.html';
+//    res.statusCode = 200;
+//    break;
+//   case '/contact-me':
+//    path += 'contact-me.html';
+//    res.statusCode = 200;
+//    break;
+//   default:
+//    path += '404.html';
+//    res.statusCode = 404;
+//    break;
+//  }
+
+//  fs.readFile(path, (err, data) => {
+//   if (err) {
+//    if (err.code !== 'ENOENT') {
+//     res.writeHead(500);
+//     res.end(`Server Error: ${err.code}`);
+//    }
+//   } else {
+//    res.end(data);
+//   }
+//  });
+// });
+
+// server.listen(8080, () => console.log("Server running on port 8080..."));
